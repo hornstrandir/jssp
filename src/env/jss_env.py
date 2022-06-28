d5ce102b52128e0d9c025cf1c1cb6512020cf57c
Paul Busch <paul@MacBook-Pro-von-Paul.local>
2022-06-28 21:40:02 +0200

    Init project setup

---
+++ src/env/jss_env.py
@@ -0,0 +1,34 @@
+class JssEnv():
+
+    def __init__(self) -> None:
+        pass
+
+    def _get_current_state_representation(self):
+        pass
+
+    def get_legal_actions(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def priorization_non_final(self):
+        pass
+
+    def _check_no_op(self):
+        pass
+
+    def step(self):
+        pass
+
+    def _reward_scaler(self):
+        pass
+
+    def increase_time_step(self):
+        pass
+
+    def _is_done(self):
+        pass
+
+    def render(self):
+        pass
